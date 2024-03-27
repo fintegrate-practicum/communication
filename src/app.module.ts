3e1a3770e87bb103e3063a545eb68787fc769c2d
saribar <nbr8041@gmail.com>
2024-03-28 01:09:41 +0200

    1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RabbitPublisherService } from './rabbit-publisher/rabbit-publisher.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, RabbitPublisherService],
 })
 export class AppModule {}
---
+++ src/rabbit-publisher/rabbit-publisher.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import * as amqp from 'amqplib';
+
+
+@Injectable()
+export class RabbitPublisherService {
+  private connection: amqp.Connection;
+  private channel: amqp.Channel;
+
+  constructor() {
+    this.connectToRabbitMQ();
+  }
+
+  async connectToRabbitMQ() {
+    this.connection = await amqp.connect('amqp://localhost');
+    this.channel = await this.connection.createChannel();
+    
+    await this.channel.assertExchange('my_exchange', 'direct', { durable: false });
+    await this.channel.assertQueue('my_queue', { durable: true });
+    await this.channel.bindQueue('my_queue', 'my_exchange', 'routing_key');
+  }
+
+  async publishMessage(routingKey: string, message: any) {
+    this.channel.publish('my_exchange', routingKey, Buffer.from(JSON.stringify(message)));
+    console.log(`Message published with routing key ${routingKey}`);  
+    console.log("hello");
+    
+  }
+}
+
+
