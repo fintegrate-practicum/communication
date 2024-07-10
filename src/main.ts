92b40d1b0bf05b3d66db93cb9728ced7d0942097
noah-cohen <n0548548106@gmail.com>
2024-07-10 10:52:29 +0200

    chech husky7

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
         password: process.env.AMQP_PASSWORD,
       },
     });
-    const g = 8;
+    const j = 8;
     microserviceApp.listen();
     const httpApp = await NestFactory.create(AppModule);
     await httpApp.listen(4156);
