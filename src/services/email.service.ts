985e4d1fecb754cdb5ed17bd7885896ab08b8483
yaely20 <y0583248523@gmail.com>
2024-07-30 13:49:41 +0300

    attachment

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -2,13 +2,11 @@ import { Injectable, Logger } from '@nestjs/common';
 import * as mailgun from 'mailgun-js';
 import { ConfigService } from '@nestjs/config';
 import { EmailLogService } from '../email-log/services/email-log.service';
-import {
-  CreateEmailLogDto,
-  UpdateEmailLogDto,
-} from '../email-log/dto/email-log.dto';
+import { CreateEmailLogDto, UpdateEmailLogDto } from '../email-log/dto/email-log.dto';
 import { EmailLogStatus } from 'src/email-log/email-log-status.enum';
 import { EmailLogDocument } from '../email-log/schemas/email-log.schema';
-
+import * as fs from 'fs';
+import * as path from 'path';
 @Injectable()
 export class EmailService {
   private readonly logger = new Logger(EmailService.name);
@@ -42,15 +40,19 @@ export class EmailService {
 
     const emailLog: EmailLogDocument =
       await this.emailLogService.create(createEmailLogDto);
-
+    const imagePath1 = path.join(__dirname, '..', '..', 'public', 'images', 'group2.png');
+    const imagePath2 = path.join(__dirname, '..', '..', 'public', 'images', 'group.png');
     const data = {
       from: this.configService.get<string>('MAILGUN_EMAIL'),
+      //   התמונות נשלחות מצורפות למייל אך לא מוצגות כראוי בגוף ההדעה
       to,
       subject,
       html,
+      attachment: [fs.createReadStream(imagePath1), fs.createReadStream(imagePath2)],
     };
 
     try {
+      this.logger.log(`Image paths: ${imagePath1}, ${imagePath2}`);
       await this.mailgun.messages().send(data);
       this.logger.log('Email sent successfully');
 
@@ -68,10 +70,7 @@ export class EmailService {
         status: EmailLogStatus.FAILED,
         errorMessage: error.message,
       };
-      await this.emailLogService.update(
-        emailLog._id.toString(),
-        updateEmailLogDto,
-      );
+      await this.emailLogService.update(emailLog._id.toString(), updateEmailLogDto);
     }
   }
   async getEmailLogById(id: string): Promise<EmailLogDocument> {
