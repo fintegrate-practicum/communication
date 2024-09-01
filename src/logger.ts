b242ec5b987980db063b9adb1300cc74bfeabd9b
SheviEngelsberg <s4161823@gmail.com>
2024-09-01 14:13:33 +0300

    update logger for cloud

--- src/logger.ts
+++ src/logger.ts
@@ -2,18 +2,29 @@ import { createLogger, format, transports, Logger } from 'winston';
 import { Papertrail } from 'winston-papertrail';
 import { LoggerService } from '@nestjs/common';
 
+// הגדרת הטרנספורטר ל-Papertrail
 const papertrailTransport = new Papertrail({
-  host: process.env.PAPERTRAIL_HOST,
-  port: process.env.PAPERTRAIL_PORT,
+  host: process.env.PAPERTRAIL_HOST || 'default_host',
+  port: parseInt(process.env.PAPERTRAIL_PORT, 10) || 12345,
   logFormat: (level: string, message: string) => `${level}: ${message}`,
 });
 
+// הגדרת הלוגר
 const logger = createLogger({
   level: 'info',
-  format: format.combine(format.timestamp(), format.simple()),
-  transports: [new transports.Console(), papertrailTransport],
+  format: format.combine(
+    format.timestamp(),
+    format.errors({ stack: true }), // מאפשר ללוג של שגיאות להוסיף את ה-stack trace
+    format.splat(), // תומך בפרמטרים מותאמים אישית
+    format.json() // תומך בפורמט JSON ללוגים
+  ),
+  transports: [
+    new transports.Console({ format: format.combine(format.colorize(), format.simple()) }),
+    papertrailTransport
+  ],
 });
 
+// מימוש של LoggerService עבור NestJS
 class PapertrailLogger implements LoggerService {
   log(message: any, ...optionalParams: any[]) {
     logger.info(message, ...optionalParams);
